54eb089b56267186581e0cf4456d4fef18c3139d
architaktus <xu_ur@yahoo.com>
2025-01-01 04:13:03 +0100

    实现GUI框架

--- modules/ui/gui.py
+++ modules/ui/gui.py
@@ -2,7 +2,7 @@
 from PyQt5.QtWidgets import (
     QMainWindow, 
     QWidget, QTabWidget,
-    QVBoxLayout, QHBoxLayout, 
+    QVBoxLayout, QHBoxLayout, QGridLayout,
     QLabel, QPushButton, QLineEdit, QCheckBox, QGroupBox, 
     QFileDialog, QFormLayout
 )
@@ -31,7 +31,13 @@ class WatermarkApp(QMainWindow):
         self.tabs.currentChanged.connect(self.update_left_panel)  # Update left panel based on the active tab
         main_layout.addWidget(self.tabs)
 
+        # 调整宽度比例
+        main_layout.setStretch(0, 2)  # 左侧面板的宽度占比为2
+        main_layout.setStretch(1, 5)  # 右侧标签页的宽度占比为5
 
+
+#####################################################################################
+#####################################################################################      
     # fixed left panel.
     def init_left_panel(self):
         left_panel = QGroupBox("Info")
@@ -41,17 +47,36 @@ class WatermarkApp(QMainWindow):
         self.preview_label = QLabel("Image Preview", self)
         self.preview_label.setAlignment(Qt.AlignCenter)
         self.preview_label.setStyleSheet("border: 1px solid grey;")
+        self.tab_label = QLabel("Filename: Embedding Mode")
         layout.addWidget(self.preview_label)
+        layout.addWidget(self.tab_label)
 
         # Info Panel
-        # exif_group = QGroupBox("Info")        TODO 改为Panel
-        self.filename_label = QLabel("Filename: None")
-        self.resolution_label = QLabel("Resolution: N/A")
-        self.exif_label = QLineEdit("Exif Data Here")
-        self.exif_label.setReadOnly(True)              # Default read-only
-        layout.addWidget(self.filename_label)
-        layout.addWidget(self.resolution_label)
-        layout.addWidget(self.exif_label)
+        exif_group = QGroupBox("Info")
+        exif_layout = QFormLayout()
+
+        self.exif_label_filename = QLineEdit("None")
+        self.exif_label_documenttitle = QLineEdit("None")
+        self.exif_label_resolution = QLabel("N/A")
+        self.exif_label_compression = QLabel("N/A")
+        self.exif_label_size = QLabel("N/A")
+        self.exif_label_colordepth = QLabel("N/A")
+        self.exif_label_createtime = QLabel("N/A")
+        self.exif_label_comment = QLineEdit("Extra Comment")
+        
+        exif_layout.addRow("file name:", self.exif_label_filename)
+        exif_layout.addRow("document title:", self.exif_label_documenttitle)
+        exif_layout.addRow("Resolution:", self.exif_label_resolution)
+        exif_layout.addRow("Compression:", self.exif_label_compression)
+        exif_layout.addRow("Size (Pixel):", self.exif_label_size)
+        exif_layout.addRow("Color Depth:", self.exif_label_colordepth)
+        exif_layout.addRow("Create Time:", self.exif_label_createtime)
+        exif_layout.addRow("Comment:", self.exif_label_comment)
+
+        exif_group.setLayout(exif_layout)
+        layout.addWidget(exif_group)
+
+
 
         # button Panel
         self.update_button = QPushButton("Reload") #重新加载Exif
@@ -63,6 +88,9 @@ class WatermarkApp(QMainWindow):
         left_panel.setLayout(layout)
         return left_panel
 
+
+#####################################################################################
+#####################################################################################
     #Initialize the Embed Watermark tab/嵌入水印的tab
     def init_embed_tab(self):
         embed_tab = QWidget()
@@ -70,7 +98,7 @@ class WatermarkApp(QMainWindow):
 
         # Image Processing Section/ 压缩
         preprocess_group = QGroupBox("Image Preprocessing")
-        preprocess_layout = QFormLayout()
+        preprocess_layout = QVBoxLayout()
         preprocess_layout.checkbox=QCheckBox("Resize to 2500px")
         preprocess_layout.checkbox.setChecked(True)
         preprocess_layout.addWidget(preprocess_layout.checkbox)
@@ -96,35 +124,43 @@ class WatermarkApp(QMainWindow):
 
         #相关固定信息
         steg_cal_group = QGroupBox("嵌入算法信息")
-        steg_cal_layout = QVBoxLayout()
+        steg_cal_layout = QFormLayout()
         self.label_alpha = QLineEdit("0.2")  #DWT Alpha
-        steg_cal_layout.addWidget(self.label_alpha)
+        steg_cal_layout.addRow("Alpha:", self.label_alpha)
         steg_cal_group.setLayout(steg_cal_layout)
         steg_layout.addWidget(steg_cal_group)
 
         #关键信息组
         steg_key_group = QGroupBox("核心信息")
-        steg_key_layout = QVBoxLayout()
+        steg_key_layout = QFormLayout()
         self.label_basic = QLineEdit("版权基本信息")  #版权基本信息
         self.label_important = QLineEdit("关键信息")  #关键信息
         self.label_extra = QLineEdit("额外信息后缀")  #额外信息后缀
-        self.label_full = QLineEdit("Label Preview")
-        self.label_full.setReadOnly(True)              # Preview only - Default read-only
+        self.label_full = QLabel("Label Preview")
         self.text_key = QLineEdit("4321")  #加密密钥
-        steg_key_layout.addWidget(self.label_basic)
-        steg_key_layout.addWidget(self.label_important)
-        steg_key_layout.addWidget(self.label_extra)
-        steg_key_layout.addWidget(self.label_full)
-        steg_key_layout.addWidget(self.text_key)
+        self.label_full_encrypted = QLabel("Encrypted Label Preview")
+        steg_key_layout.addRow("版权基本信息:", self.label_basic)
+        steg_key_layout.addRow("关键信息:", self.label_important)
+        steg_key_layout.addRow("额外信息后缀:", self.label_extra)
+        steg_key_layout.addRow("完整水印预览:", self.label_full)
+        steg_key_layout.addRow("加密密钥:", self.text_key)
+        steg_key_layout.addRow("加密水印预览:", self.label_full_encrypted)
         steg_key_group.setLayout(steg_key_layout)
         steg_layout.addWidget(steg_key_group)
 
         #随机数组
         steg_sub_group = QGroupBox("辅助信息")
-        steg_sub_layout = QVBoxLayout()
+        steg_sub_layout = QFormLayout()
         self.text_seed = QLineEdit("1234")  #随机数seed
+        self.text_lenght = QLineEdit("10")  #随机数长度
+        steg_sub_layout.random_text = QLabel("N/A")
+        steg_sub_layout.checkbox_random_text = QCheckBox()
+        steg_sub_layout.checkbox_random_text.setChecked(True)        
+        steg_sub_layout.addRow("随机数seed:", self.text_seed)
+        steg_sub_layout.addRow("随机数长度:", self.text_lenght)
+        steg_sub_layout.addRow("随机数:", steg_sub_layout.random_text)
+        steg_sub_layout.addRow("自动生成随机数", steg_sub_layout.checkbox_random_text)
         steg_sub_layout.addWidget(QPushButton("伪随机数生成"))  #伪随机数生成按钮
-        steg_sub_layout.addWidget(self.text_seed)
         steg_sub_group.setLayout(steg_sub_layout)
         steg_layout.addWidget(steg_sub_group)
 
@@ -140,7 +176,8 @@ class WatermarkApp(QMainWindow):
 
 
 
-
+#####################################################################################
+#####################################################################################
     #Initialize the Read Watermark tab/读取水印的tab
     def init_read_tab(self):
         read_tab = QWidget()
@@ -149,41 +186,53 @@ class WatermarkApp(QMainWindow):
 
         # QR Code Section
         qr_group = QGroupBox("QR Code")
-        qr_layout = QVBoxLayout()
+        qr_layout = QGridLayout()
+            # 左侧：二维码图片预览框
         self.qr_preview_pic = QLabel("QR Code Pic Preview")
-        self.qr_preview_pic.setAlignment(Qt.AlignLeft)
-        self.qr_decode_text = QLabel("QR Code decode Preview")
+        self.qr_preview_pic.setAlignment(Qt.AlignCenter)
+        self.qr_preview_pic.setStyleSheet("border: 1px solid grey;")
+        qr_layout.addWidget(self.qr_preview_pic, 0, 0, 3, 1)  # 第一行，第一列，至第3行，第1列（起始坐标x,y, rowspan, colspan）
+            # 右侧：二维码内容预览
+        qr_layout.qr_decode_title = QLabel("QR Code decode Preview:")
+        self.qr_decode_text = QLabel("N/A")
+        qr_layout.addWidget(qr_layout.qr_decode_title, 0, 1)   # 第一行，第2列
+        qr_layout.addWidget(self.qr_decode_text, 1, 1)   # 第2行，第2列
+            # 右侧：解码按钮
         self.button_qr_decode = QPushButton("Decode QR Code")  #解析
-        #qr_layout.button_qr_decode.setAlignment(Qt.AlignRight)
-        qr_layout.addWidget(self.qr_preview_pic)
-        qr_layout.addWidget(self.qr_decode_text)
-        qr_layout.addWidget(self.button_qr_decode)
+        qr_layout.addWidget(self.button_qr_decode, 2, 1)   # 第3行，第2列
+            # 调整行列拉伸因子、排列
+        #qr_layout.setRowStretch(0, 1)
+        #qr_layout.setRowStretch(1, 1)
+        #qr_layout.setRowStretch(2, 5)
+
         qr_group.setLayout(qr_layout)
         layout.addWidget(qr_group)
+        layout.setStretch(0, 3)
+
 
         # 文本水印
         watermark_text_group = QGroupBox("文本水印")
-        watermark_text_layout = QVBoxLayout()
-        self.text_preview = QLineEdit("加密文字内容")
-        self.text_preview.setReadOnly(True)              # Preview only - Default read-only
+        watermark_text_layout = QFormLayout()
+        self.text_preview = QLabel("N/A")
         self.text_key = QLineEdit("4321")  #加密密钥 来自database
-        watermark_text_layout.addWidget(self.text_key)
-        watermark_text_layout.addWidget(self.text_preview)
-        watermark_text_layout.addWidget(QPushButton("Extract Watermark"))
+        watermark_text_layout.addRow("加密密钥:", self.text_key)
+        watermark_text_layout.addRow("加密文字内容:", self.text_preview)
+        #watermark_text_layout.addWidget(QPushButton("Extract Watermark"))
         watermark_text_group.setLayout(watermark_text_layout)
         layout.addWidget(watermark_text_group)
+        layout.setStretch(1, 1)
+
 
         # 辅助水印
         watermark_sub_group = QGroupBox("辅助水印")
-        watermark_sub_layout = QVBoxLayout()
-        self.random_preview = QLineEdit("伪随机数")
-        self.random_preview.setReadOnly(True)              # Preview only - Default read-only
-        self.random_seed = QLineEdit("4321")  #随机数seed 来自database
-        self.random_seed.setReadOnly(True)              # Preview only - Default read-only
-        watermark_sub_layout.addWidget(self.random_preview)
-        watermark_sub_layout.addWidget(self.random_seed)
+        watermark_sub_layout = QFormLayout()
+        self.random_preview = QLabel("N/A")
+        self.random_seed = QLabel("4321")  #随机数seed 来自database
+        watermark_sub_layout.addRow("随机数seed:", self.random_seed)
+        watermark_sub_layout.addRow("伪随机数:", self.random_preview)
         watermark_sub_group.setLayout(watermark_sub_layout)
         layout.addWidget(watermark_sub_group)
+        layout.setStretch(2, 1)
 
 
         # Buttons
@@ -191,6 +240,7 @@ class WatermarkApp(QMainWindow):
         self.button_decode = QPushButton("Decode Watermarks")  #水印读取
         layout.addWidget(self.button_reload)
         layout.addWidget(self.button_decode)
+        layout.setStretch(3, 1) 
 
         read_tab.setLayout(layout)
         return read_tab
@@ -200,11 +250,11 @@ class WatermarkApp(QMainWindow):
     def update_left_panel(self):
         current_tab = self.tabs.currentIndex()
         if current_tab == 0:  # Embed Watermark tab
-            self.exif_label.setReadOnly(False)
-            self.filename_label.setText("Filename: Embedding Mode")
+            #self.tab_label.setReadOnly(False)
+            self.tab_label.setText("Filename: Embedding Mode")
         elif current_tab == 1:  # Read Watermark tab
-            self.exif_label.setReadOnly(True)
-            self.filename_label.setText("Filename: Reading Mode")
+            #self.tab_label.setReadOnly(True)
+            self.tab_label.setText("Filename: Reading Mode")
 
     def open_image(self):
         file_name, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Images (*.png *.jpg *.jpeg)")
